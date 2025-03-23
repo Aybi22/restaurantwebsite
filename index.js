6fb886efa5146fb024246a3a9f2f7da6c535c687
Aybi22 <your_email_address@example.com>
2025-03-23 12:27:19 +0000

    "updating pages content"

--- index.js
+++ index.js
@@ -4,5 +4,40 @@ fetch('./template.html')
     let temp=document.getElementById('temp');
     
     temp.innerHTML=html;
-   
+
+    const modal=document.querySelector('.modal');
+    const hamburger=document.querySelector('.fa-bars');
+    const closer=document.querySelector('.close-modal');
+    
+    hamburger.addEventListener('click',displayModal);
+    function displayModal(){
+    modal.classList.toggle('show-modal');
+   hamburger.classList.add('red');
+    }
+
+
+closer.addEventListener('click',closeModal);
+ function closeModal(e){
+let x=e.target.parentNode;
+
+
+
+ 
+  x.classList.toggle('show-modal');
+  
+
+
+}
+    
+ 
+
+
+
+
+
+
+
+
+
+
 });
